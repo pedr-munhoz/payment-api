6a2790d31b0284969ef0dcf7e26571e756f2a7d2
Pedro Munhoz <pedr.munhoz@gmail.com>
2020-10-26 23:12:05 -0300

    StableV1 migration applied.

---
+++ Migrations/Server/20201027021114_StableV1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace payment_api.Migrations.Server
+{
+    public partial class StableV1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AnticipationAnalyses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StartDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    EndDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    FinalStatus = table.Column<string>(type: "text", nullable: true),
+                    AnticipationId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AnticipationAnalyses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Anticipations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SolicitationDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    SolicitedValue = table.Column<double>(type: "double precision", nullable: false),
+                    AnticipatedValue = table.Column<double>(type: "double precision", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Anticipations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Installments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PaymentId = table.Column<int>(type: "integer", nullable: false),
+                    RawValue = table.Column<double>(type: "double precision", nullable: false),
+                    LiquidValue = table.Column<double>(type: "double precision", nullable: false),
+                    InstallmentNumber = table.Column<int>(type: "integer", nullable: false),
+                    AnticipatedValue = table.Column<double>(type: "double precision", nullable: true),
+                    DueDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    AnticipatedTranferDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Installments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TransactionDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    AprovalDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    CancelDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    Anticipated = table.Column<bool>(type: "boolean", nullable: true),
+                    Approved = table.Column<bool>(type: "boolean", nullable: false),
+                    RawValue = table.Column<double>(type: "double precision", nullable: false),
+                    LiquidValue = table.Column<double>(type: "double precision", nullable: false),
+                    Tax = table.Column<double>(type: "double precision", nullable: false),
+                    CreditCard = table.Column<string>(type: "text", nullable: true),
+                    PaymentInstallmentCount = table.Column<int>(type: "integer", nullable: false),
+                    AnticipationId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AnticipationAnalyses");
+
+            migrationBuilder.DropTable(
+                name: "Anticipations");
+
+            migrationBuilder.DropTable(
+                name: "Installments");
+
+            migrationBuilder.DropTable(
+                name: "Payments");
+        }
+    }
+}
